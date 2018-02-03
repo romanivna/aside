280fa51de36a758c6ba322920774d78252fa4ece
romanivna <m.t.romanivna@gmail.com>
2018-02-03 21:43:35 +0200

    map

--- js/map.js
+++ js/map.js
@@ -1,7 +1,8 @@
     /*google maps*/
-window.onload = function () {
+    console.log('ok')
+$(document).ready(function () {
 var LatLng = {lat: 43.520039, lng: -96.738216};
-
+console.log(LatLng)
 var map = new google.maps.Map(document.getElementById('map'), {
 zoom: 16,
 center: LatLng
@@ -12,4 +13,4 @@ position: LatLng,
 map: map,
 title: 'Random coords'
 });
-};
+})
